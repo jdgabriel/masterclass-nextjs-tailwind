dc9ffc046444538a4f6040d4e4069abb76605632
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2023-11-16 09:45:34 -0300

    used space widget

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,12 @@
 import { Logo } from '@/components/Logo'
-import { Search } from 'lucide-react'
+import { Cog, LifeBuoy, Search } from 'lucide-react'
 import { MainNavigation } from './MainNavigation'
+import { NavItem } from './MainNavigation/NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <div className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <div className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-1 flex w-full items-start gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -18,6 +20,15 @@ export function Sidebar() {
       </div>
 
       <MainNavigation />
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </div>
   )
 }
