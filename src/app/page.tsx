75b75b81d46b84e2ad6dfd6787a8e4d96882c54c
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2023-11-16 17:25:30 -0300

    create button variant with tailwind-variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { SettingsTab } from '@/components/SettingsTab'
 import { Select } from '@/components/Form/Select'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { Textarea } from '@/components/Form/Textarea'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -20,19 +21,12 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
-              className="rounded-lg border border-zinc-300 px-4 py-2 font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-              type="button"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              className="rounded-lg bg-violet-600 px-4 py-2 font-semibold text-white  shadow-sm hover:bg-violet-700"
-              type="submit"
-              form="form-settings"
-            >
+            </Button>
+            <Button type="submit" form="form-settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -204,18 +198,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              className="rounded-lg border border-zinc-300 px-4 py-2 font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-              type="button"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              className="rounded-lg bg-violet-600 px-4 py-2 font-semibold text-white  shadow-sm hover:bg-violet-700"
-              type="submit"
-            >
+            </Button>
+            <Button type="submit" form="form-settings">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
