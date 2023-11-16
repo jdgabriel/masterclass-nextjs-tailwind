c341461bdc94e2b9a1d9e38383210ac8dca8f455
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2023-11-16 17:21:58 -0300

    create focus and accessibility inputs

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -11,7 +11,7 @@ type InputControlProps = ComponentProps<'input'>
 export function InputControl(props: InputControlProps) {
   return (
     <input
-      className="w-full border-0 bg-transparent p-0 text-zinc-700 placeholder-zinc-400"
+      className="w-full border-0 bg-transparent p-0 text-zinc-700 placeholder-zinc-400 outline-none"
       {...props}
     />
   )
@@ -21,7 +21,7 @@ type InputRootProps = ComponentProps<'div'>
 export function InputRoot(props: InputRootProps) {
   return (
     <div
-      className="flex w-full items-start gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
       {...props}
     />
   )
